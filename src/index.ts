63693f9b027ee2eb4057e22e72a70576f0c00318
Guillaume Munsch <guillaume.munsch@ovrsea.com>
2023-10-23 16:48:19 +0200

    With factory

--- src/index.ts
+++ src/index.ts
@@ -9,22 +9,24 @@ const logItemsWithLabel = (label: string, items: NewItemInterface[]) => {
   console.log(`${label}: [${items.map(({ generateItemDetails }) => generateItemDetails()).join("\n - \n")}]`);
 };
 
+const itemFactory = ({ name, quality, sellIn }: Item) => {
+  switch (name) {
+    case "Sulfuras, Hand of Ragnaros":
+      return new SulfurasItem();
+    case "Aged Brie":
+      return new AgedBrie(name, sellIn, quality);
+    case "Backstage passes to a TAFKAL80ETC concert":
+      return new BackstagePass(name, sellIn, quality);
+    default:
+      return new LegacyItem(name, sellIn, quality);
+  }
+};
+
 export class GildedRose {
   items: NewItemInterface[];
 
   constructor(items: Array<Item> = []) {
-    this.items = items.map(({ name, quality, sellIn }) => {
-      if (name === "Sulfuras, Hand of Ragnaros") {
-        return new SulfurasItem();
-      }
-      if (name === "Aged Brie") {
-        return new AgedBrie(name, sellIn, quality);
-      }
-      if (name === "Backstage passes to a TAFKAL80ETC concert") {
-        return new BackstagePass(name, sellIn, quality);
-      }
-      return new LegacyItem(name, sellIn, quality);
-    });
+    this.items = items.map(itemFactory);
     logItemsWithLabel("Items on construction", this.items);
   }
 
